b63ff567dc11b6334ccd9b6ce6e23c46e75471f8
Catherine <catherinetong@outlook.com>
2018-12-05 17:06:30 +0000

    .

--- gcn/utils.py
+++ gcn/utils.py
@@ -231,12 +231,14 @@ def performance_per_group(nodes_per_group, nodes_per_cpn, test_o_acc_all, data_s
 
     cpn_sizes = list(set([cname[0] for cname in nodes_per_cpn]))
 
+    print('=========per SIZE===========')
+
     for phase in ['train', 'val', 'test']:
         idx = data_split['idx_' + phase]
-        print('----{}----')
+        print('----{}----'.format(phase))
         for cpn_size in nodes_per_group:
             num_in_phase = len([n for n in nodes_per_group[cpn_size] if n in idx])
-            print('size {} : {} ({}%)'.format(cpn_size, num_in_phase, num_in_phase/ len(idx))*100)
+            print('size {} : {} ({}%)'.format(cpn_size, num_in_phase, num_in_phase/ len(idx)*100))
 
             acc_of_cpn_size = np.mean([test_per_node[node] for node in idx_test if node in nodes_per_group[cpn_size]])
             print('accuracy = ', acc_of_cpn_size)
@@ -249,8 +251,8 @@ def performance_per_group(nodes_per_group, nodes_per_cpn, test_o_acc_all, data_s
             num_in_phase = len([n for n in nodes_per_cpn[cpn] if n in idx])
             print('{} : {} ({}%)'.format(phase, num_in_phase, num_in_phase/ len(nodes_per_cpn[cpn])))
 
-            acc_of_cpn = np.mean([test_per_node[node] for node in idx_test if node in nodes_per_cpn[cpn]])
-            print('accuracy = ', acc_of_cpn)
+        acc_of_cpn = np.mean([test_per_node[node] for node in idx_test if node in nodes_per_cpn[cpn]])
+        print('accuracy = ', acc_of_cpn)
 
     # acc_per_group = {}
     # for cpn_size in nodes_per_group:
